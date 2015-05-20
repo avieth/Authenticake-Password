ed8b47177b0387b9a43c12b2ab522138b78466fb
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2015-05-20 01:25:34 -0400

    Overhaul: use bcrypt, relational interpreter
    This makes the project finally useful. Any suitable relational
    interpreter can act as a storage medium for username, bcrypt digest
    pairs.

--- Authenticake/Password.hs
+++ Authenticake/Password.hs
@@ -1,50 +1,120 @@
+{-|
+Module      : Authenticake.Password
+Description : Non-volatile password auth using bcrypt and an RDBMS.
+Copyright   : (c) Alexander Vieth, 2015
+Licence     : BSD3
+Maintainer  : aovieth@gmail.com
+Stability   : experimental
+Portability : non-portable (GHC only)
+-}
+
+{-# LANGUAGE AutoDeriveTypeable #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE ConstraintKinds #-}
 
 module Authenticake.Password (
 
-    PasswordAuthenticator(..)
-  , DigestComparison(..)
-  , PasswordAuthenticationFailure(..)
+    PasswordAuthenticator
+  , password
+
+  , HashingPolicy(..)
+  , fastBcryptHashingPolicy
+  , slowerBcryptHashingPolicy
 
   ) where
 
+import Control.Monad.IO.Class
+import qualified Data.Text as T
+import qualified Data.ByteString as BS
+import Data.Proxy
+import Data.Relational
+import Data.Relational.Universe
+import Data.Relational.Interpreter
+import Crypto.BCrypt
 import Authenticake.Authenticate
+import Authenticake.Secret
 
-data PasswordAuthenticator m s c d = PasswordAuthenticator {
-    getDigest :: s -> m (Maybe d)
-  , setDigest :: s -> Maybe c -> m ()
-  , compareDigest :: s -> c -> d -> DigestComparison
-  }
+type SubjectColumn = '("subject", T.Text)
+type DigestColumng = '("digest", BS.ByteString)
+type PasswordSchema = '[ SubjectColumn, DigestColumng ]
 
-data DigestComparison = Match | NoMatch
+subjectColumn :: Column SubjectColumn
+subjectColumn = column
 
-data PasswordAuthenticationFailure
-  = BadPassword
-  | SubjectNotFound
-  deriving (Show)
+digestColumn :: Column DigestColumng
+digestColumn = column
 
-instance
-    ( Monad m
-    )
-    => Authenticator (PasswordAuthenticator m s c d)
+passwordSchema :: Schema PasswordSchema
+passwordSchema = subjectColumn :| digestColumn :| EndSchema
+
+passwordTable :: Table '("password", PasswordSchema)
+passwordTable = Table Proxy passwordSchema
+
+subjectCondition :: T.Text -> Condition '[ '[ SubjectColumn ] ]
+subjectCondition subject = subjectColumn .==. subject .||. false .&&. true
+
+challengeProjection :: Project '[ DigestColumng ]
+challengeProjection = digestColumn :+| EndProject
+
+makeRow :: T.Text -> BCryptDigest -> Row PasswordSchema
+makeRow subject digest =
+      (fromColumnAndValue subjectColumn subject)
+  :&| (fromColumnAndValue digestColumn digest)
+  :&| EndRow
+
+type PasswordAuthenticator i = SecretAuthenticator (InterpreterMonad i) T.Text BS.ByteString BCryptDigest
+
+type BCryptDigest = BS.ByteString
+
+-- | A password authenticator using a particular relational interpreter
+--   and hashing policy.
+--   The existence of a table corresponding to passwordTable is assumed.
+password
+  :: forall i .
+     ( RelationalInterpreter i
+     , Functor (InterpreterMonad i)
+     , Monad (InterpreterMonad i)
+     , MonadIO (InterpreterMonad i)
+     , InterpreterSelectConstraint i PasswordSchema '[DigestColumng] '[ '[SubjectColumn] ]
+     , InterpreterInsertConstraint i PasswordSchema
+     , InterpreterDeleteConstraint i PasswordSchema '[ '[SubjectColumn] ]
+     , InUniverse (Universe i) BS.ByteString
+     , InUniverse (Universe i) T.Text
+     )
+  => Proxy i
+  -> HashingPolicy
+  -> PasswordAuthenticator i
+password proxy policy = SecretAuthenticator getDigest setDigest checkDigest
 
   where
 
-    type NotAuthenticReason (PasswordAuthenticator m s c d) t = PasswordAuthenticationFailure
-    type Subject (PasswordAuthenticator m s c d) t = s
-    type Challenge (PasswordAuthenticator m s c d) t = c
-    type AuthenticatorF (PasswordAuthenticator m s c d) = m
+    getDigest :: T.Text -> (InterpreterMonad i) (Maybe BCryptDigest)
+    getDigest subject = do
+        rows <- interpretSelect' proxy (Select passwordTable challengeProjection (subjectCondition subject))
+        let rows' = rows >>= maybe [] return
+        case rows' of
+            [] -> return Nothing
+            (digest :&| EndRow) : _ -> return (Just (fieldValue digest))
 
-    authenticate authenticator proxy subject challenge = do
-        maybeExistingDigest <- getDigest authenticator subject
-        case maybeExistingDigest of
-          Nothing -> return (Just SubjectNotFound)
-          Just existingDigest ->
-              case compareDigest authenticator subject challenge existingDigest of
-                  Match -> return Nothing
-                  NoMatch -> return (Just BadPassword)
+    setDigest :: T.Text -> Maybe BS.ByteString -> (InterpreterMonad i) ()
+    setDigest subject mchallenge = do
+        interpretDelete proxy (Delete passwordTable (subjectCondition subject))
+        case mchallenge of
+            Nothing -> return ()
+            Just challenge -> do
+                mdigest <- liftIO (hashPasswordUsingPolicy policy challenge)
+                case mdigest of
+                    Nothing -> error "BCrypt failed to generate a digest!"
+                    Just digest -> interpretInsert proxy (Insert passwordTable (makeRow subject digest))
 
+    checkDigest :: T.Text -> BS.ByteString -> BCryptDigest -> (InterpreterMonad i) SecretComparison
+    checkDigest subject challenge digest = do
+        if not (validatePassword digest challenge)
+        then return NoMatch
+        else if hashUsesPolicy policy digest
+             then return Match
+             else setDigest subject (Just challenge) >> return Match
